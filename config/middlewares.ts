0b5a851229538c246264cfb72a965820c7ea2541
liam welling <liamwellingg36@gmail.com>
2025-09-01 10:48:10 -0400

    thumbnail error

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,17 @@
-export default [
+  module.exports = ({ env }) => [
   'strapi::logger',
-  'strapi::errors',
+    "strapi::errors",
+      {
+        name: "strapi::security",
+        config: {
+          contentSecurityPolicy: {
+            directives: {
+              "connect-src": ["'self'", "http:", env("SUPABASE_URL")], // Adjust as needed
+              "img-src": ["'self'", "data:", "blob:", env("SUPABASE_URL"), "https://dl.airtable.com"], // Adjust as needed
+            },
+          },
+        },
+      },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
